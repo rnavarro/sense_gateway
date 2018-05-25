6e636607b711f8c0ac40adde308435a0f1940993
Robert Navarro <rnavarro@stitchlabs.com>
2018-05-25 04:42:16 -0700

    Add fix for timezone

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from sense_energy import Senseable
 # https://pvoutput.org/help.html#api-spec
 PV_OUTPUT_STATUS_URL = 'https://pvoutput.org/service/r2/addstatus.jsp'
 
+API_URL = 'https://api.sense.com/apiservice/api/v1/'
+API_TIMEOUT = 1
+
 config = configparser.ConfigParser()
 config.read('config.ini')
 
@@ -17,12 +20,24 @@ sense_password = config.get('sense_credentials', 'password')
 
 sense = Senseable(sense_username, sense_password)
 
+
+def get_daily_usage():
+    response = sense.s.get(API_URL + 'app/history/trends?monitor_id=%s&scale=DAY&start=%s' %
+                           (sense.sense_monitor_id, datetime.utcnow().isoformat()[:-3] + 'Z'),
+                           headers=sense.headers, timeout=API_TIMEOUT)
+    data = response.json()
+
+    return data
+
+
+daily_usage = get_daily_usage()
+
 print(datetime.isoformat(' '))
 print("Active:", sense.active_power, "W")
 print("Active Solar:", sense.active_solar_power, "W")
 print("Active Devices:", ", ".join(sense.active_devices))
-print("Daily Usage:", sense.daily_usage, "kWh")
-print("Daily Production:", sense.daily_production, "kWh")
+print("Daily Usage:", daily_usage['consumption']['total'], "kWh")
+print("Daily Production:", daily_usage['production']['total'], "kWh")
 
 pvoutput_apikey = config.get('pvoutput_credentials', 'api_key')
 pvoutput_system_id = config.get('pvoutput_credentials', 'system_id')
@@ -38,9 +53,9 @@ pvoutput_request_headers = {
 pvoutput_request_data = {
     'd':  date,
     't':  time,
-    # 'v1': sense.daily_production * 1000,
+    # 'v1': daily_usage['production']['total'] * 1000,
     # 'v2': sense.active_solar_power,
-    'v3': sense.daily_usage * 1000,
+    'v3': daily_usage['consumption']['total'] * 1000,
     'v4': sense.active_power,
 }
 
